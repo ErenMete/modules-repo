f39f3a66fe061a5361fd5082e0932bba296f26e0
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-11-26 19:25:37 +0000

    spam: fix mistakes

--- spam.py
+++ spam.py
@@ -43,15 +43,13 @@ class SpamMod(loader.Module):
         args = utils.get_args(message)
         logger.debug(args)
         if len(args) == 0:
-            ret = self.strings["need_spam"]
-            await message.edit(ret)
+            await utils.answer(message, self.strings["need_spam"])
             return
         if len(args) == 1:
             if message.is_reply:
                 use_reply = True
             else:
-                ret = self.strings["spam_urself"]
-                await message.edit(ret)
+                await utils.answer(message, self.strings["spam_urself"])
                 return
         count = args[0]
         spam = (await message.get_reply_message()) if use_reply else message
@@ -59,12 +57,10 @@ class SpamMod(loader.Module):
         try:
             count = int(count)
         except ValueError:
-            ret = self.strings["nice_number"]
-            await message.edit(ret)
+            await utils.answer(message, self.strings["nice_number"])
             return
         if count < 1:
-            ret = self.strings["much_spam"]
-            await message.edit(ret)
+            await utils.answer(message, self.strings["much_spam"])
             return
         await message.delete()
         if count > 20:
